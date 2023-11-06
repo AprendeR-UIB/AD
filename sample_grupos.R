09e831f893a9c955fd9a625ca597f1881ef01f78
igmuib <igmuib@uib.es>
2023-11-06 13:50:28 +0100

    ACP

--- sample_grupos.R
+++ sample_grupos.R
@@ -1,13 +1,16 @@
-grupo1<-c("Ángel García", "Arnau Piferrer", "Guillem Serra") #1
-grupo2<-c("Carla Martínez", "Patricia Mateu", "Margalida Verd") #6
-grupo3<-c("Javi Torres", "Marcos Gibert") #2
-grupo4<-c("Ángel Álamo", "Juanjo Doblas", "Óscar Vanrell") # 7
-grupo5<-c("Maria Àngels Llobera", "Alba Linares") # 8
-grupo6<-c("Marcos Oliva","Hugo García") #3
-grupo7<-c("Irene Julià", "Xisco Ribera Ramis") #5
+grupo1<-c("Ángel García", "Arnau Piferrer", "Guillem Serra") #
+grupo2<-c("Carla Martínez", "Patricia Mateu", "Margalida Verd") #
+grupo3<-c("Javi Torres", "Marcos Gibert") #
+grupo4<-c("Ángel Álamo", "Juanjo Doblas", "Óscar Vanrell") # 
+grupo5<-c("Maria Àngels Llobera", "Alba Linares") # 
+grupo6<-c("Marcos Oliva","Hugo García") #
+grupo7<-c("Irene Julià", "Xisco Ribera Ramis") #
 grupo8<-c("Maria del Mar Deyà", "Toni Fuentes Bauzà", "Frank Hammond") #4
 
-set.seed(91023)
+set.seed(31102023)
 sample(1:8,8)
 
-#Exposición 23 y 25 de octubre. 
+#Exposición 13 y 15 de noviembre. 
+
+> sample(1:8,8)
+[1] 3 6 7 2 8 1 5 4
