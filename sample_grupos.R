c18544bd751c4794336cc6e694a7cc7dbde881ce
igmuib <igmuib@uib.es>
2023-11-13 13:01:02 +0100

    MDS

--- sample_grupos.R
+++ sample_grupos.R
@@ -1,11 +1,11 @@
-grupo1<-c("Ángel García", "Arnau Piferrer", "Guillem Serra") #
-grupo2<-c("Carla Martínez", "Patricia Mateu", "Margalida Verd") #
-grupo3<-c("Javi Torres", "Marcos Gibert") #
-grupo4<-c("Ángel Álamo", "Juanjo Doblas", "Óscar Vanrell") # 
-grupo5<-c("Maria Àngels Llobera", "Alba Linares") # 
-grupo6<-c("Marcos Oliva","Hugo García") #
-grupo7<-c("Irene Julià", "Xisco Ribera Ramis") #
-grupo8<-c("Maria del Mar Deyà", "Toni Fuentes Bauzà", "Frank Hammond") #4
+grupo1<-c("Ángel García", "Arnau Piferrer", "Guillem Serra") # 6
+grupo2<-c("Carla Martínez", "Patricia Mateu", "Margalida Verd") # 4
+grupo3<-c("Javi Torres", "Marcos Gibert") # 1
+grupo4<-c("Ángel Álamo", "Juanjo Doblas", "Óscar Vanrell") # 8
+grupo5<-c("Maria Àngels Llobera", "Alba Linares") # 7
+grupo6<-c("Marcos Oliva","Hugo García") # 2
+grupo7<-c("Irene Julià", "Xisco Ribera Ramis") # 3
+grupo8<-c("Maria del Mar Deyà", "Toni Fuentes Bauzà", "Frank Hammond") # 5
 
 set.seed(31102023)
 sample(1:8,8)
