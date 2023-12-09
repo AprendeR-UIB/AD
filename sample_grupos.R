145dae78fdbd05e0a14c37b5e0d2d7bbcf6dc040
igmuib <irene.garcia@uib.es>
2023-12-09 17:45:30 +0100

    Actualizacion

--- sample_grupos.R
+++ sample_grupos.R
@@ -1,16 +1,15 @@
-grupo1<-c("Ángel García", "Arnau Piferrer", "Guillem Serra") # 6
-grupo2<-c("Carla Martínez", "Patricia Mateu", "Margalida Verd") # 4
-grupo3<-c("Javi Torres", "Marcos Gibert") # 1
-grupo4<-c("Ángel Álamo", "Juanjo Doblas", "Óscar Vanrell") # 8
-grupo5<-c("Maria Àngels Llobera", "Alba Linares") # 7
-grupo6<-c("Marcos Oliva","Hugo García") # 2
-grupo7<-c("Irene Julià", "Xisco Ribera Ramis") # 3
-grupo8<-c("Maria del Mar Deyà", "Toni Fuentes Bauzà", "Frank Hammond") # 5
+grupo1<-c("Ángel García", "Arnau Piferrer", "Guillem Serra") # 4 no Ángel
+grupo2<-c("Carla Martínez", "Patricia Mateu", "Margalida Verd") # 3 no Marga
+grupo3<-c("Javi Torres", "Marcos Gibert") # 1 expone Javi
+grupo4<-c("Ángel Álamo", "Juanjo Doblas", "Óscar Vanrell") # No Juanjo
+grupo5<-c("Maria Àngels Llobera", "Alba Linares") # 5 Maria Angels
+grupo6<-c("Marcos Oliva","Hugo García") # 7  Marcos
+grupo7<-c("Irene Julià", "Xisco Ribera Ramis") # 2 Xisco
+grupo8<-c("Maria del Mar Deyà", "Toni Fuentes Bauzà", "Frank Hammond") # 6 No Frank
 
-set.seed(31102023)
+set.seed(04122023)
 sample(1:8,8)
 
-#Exposición 13 y 15 de noviembre. 
+#Exposición 18 y 20 de diciembre. 
 
-> sample(1:8,8)
-[1] 3 6 7 2 8 1 5 4
+sample(1:8,8)
